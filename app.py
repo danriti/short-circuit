658a1298e8d3c1645ed4b483e75d0a1b684fd162
Dan Riti <dmriti@gmail.com>
2015-09-09 23:29:18 +0000

    Update get_time to use a circuit breaker.

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 """ app.py """
 
 from flask import Flask, render_template
+import pybreaker
 import requests
 
 
 app = Flask(__name__)
+time_breaker = pybreaker.CircuitBreaker(fail_max=3, reset_timeout=30)
 
 
-def get_time():
+@time_breaker
+def _get_time():
     try:
         response = requests.get('http://localhost:3001/time', timeout=3.0)
     except (requests.exceptions.ConnectionError,
             requests.exceptions.Timeout):
-        return 'Unavailable'
+        # please note that this is an example implementation and is
+        # dangerously masking the original exception. i highly recommend
+        # you take the appropriate means to capture/log the exception so
+        # that you are aware of the underlying problem that is triggering
+        # the circuit breaker!
+        raise pybreaker.CircuitBreakerError
     return response.json().get('datetime')
+
+
+def get_time():
+    try:
+        return _get_time()
+    except pybreaker.CircuitBreakerError:
+        return 'Unavailable'
 
 
 def get_user():
